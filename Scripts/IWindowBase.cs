164f8ff4542157f98b7ee025755fccee4790b2cb
FirstSymbol <firstsymbol.0@gmail.com>
2025-12-14 23:02:38 +0300

    [ADD] Force option [MOD] Extract interface into a separate class

---
+++ Scripts/IWindowBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace WindowsSystem
+{
+  public interface IWindowBase
+  {
+    public Action<Type> OnBeforeShow { get; set; }
+    public Action<Type> OnBeforeHide { get; set; }
+    public Action<Type> OnAfterShow { get; set; }
+    public Action<Type> OnAfterHide { get; set; }
+    public UniTask Show(bool isForce = false);
+    public UniTask Hide(bool isForce = false);
+    public void Toggle();
+    public bool IsShowing { get; }
+    public bool InQueue { get; set; }
+    public IWindowsService WindowService { get; }
+    public int QueuePriority { get; }
+    public bool IsInteractable { get; }
+    public void EnableInteract();
+    public void DisableInteract();
+    public void ToggleInteract();
+  }
+}
