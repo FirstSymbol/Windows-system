0d024f015a739d1e4fe900eaf290498b873851ee
FirstSymbol <firstsymbol.0@gmail.com>
2026-01-16 11:48:42 +0300

    Spawning provider

---
+++ Scripts/Providers/IWindowsProvider.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using WindowsSystem;
+
+namespace Windows_system.Scripts.Providers
+{
+  public interface IWindowsProvider
+  {
+    public T GetWindowPrefab<T>() where T : MonoBehaviour, IWindowBase;
+  }
+}
