73d1077400ff6c61fc91205c08b7edf49a703fb7
FirstSymbol <firstsymbol.0@gmail.com>
2026-02-09 11:48:46 +0300

    Add default provider

---
+++ Scripts/Providers/WindowsProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WindowsSystem.Providers
+{
+  [CreateAssetMenu(fileName = "WindowsProvider Config", menuName = "Configs/Providers/WindowsSystem/WindowsProvider", order = 0)]
+  public class WindowsProvider : ScriptableObject, IWindowsProvider, ISerializationCallbackReceiver
+  {
+    public List<GameObject> windowsObjects;
+    private Dictionary<Type, IWindowBase>  _windows = new();
+    
+    public void OnBeforeSerialize()
+    {
+      windowsObjects = windowsObjects.Distinct().ToList();
+      _windows ??= new Dictionary<Type, IWindowBase>();
+      _windows.Clear();
+      
+      for (int i = 0; i < windowsObjects.Count; i++)
+      {
+        if (windowsObjects[i] == null)
+          continue;
+        if (windowsObjects[i].TryGetComponent(out IWindowBase window))
+        {
+          _windows.Add(window.GetType(), window);
+        }
+        else
+          windowsObjects[i] = null;
+      }
+    }
+
+    public void OnAfterDeserialize()
+    {
+      
+    }
+    
+    public T GetWindowPrefab<T>() where T : MonoBehaviour, IWindowBase
+    {
+      return _windows[typeof(T)] as T;
+    }
+
+    
+  }
+}
