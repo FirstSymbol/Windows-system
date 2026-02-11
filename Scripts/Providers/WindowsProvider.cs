f64825f58c7441fa78063d64da2d59cea6c73275
FirstSymbol <firstsymbol.0@gmail.com>
2026-02-11 17:12:32 +0300

    Provider

--- Scripts/Providers/WindowsProvider.cs
+++ Scripts/Providers/WindowsProvider.cs
@@ -5,41 +5,40 @@ using UnityEngine;
 
 namespace WindowsSystem.Providers
 {
-  [CreateAssetMenu(fileName = "WindowsProvider Config", menuName = "Configs/Providers/WindowsSystem/WindowsProvider", order = 0)]
-  public class WindowsProvider : ScriptableObject, IWindowsProvider, ISerializationCallbackReceiver
+  [CreateAssetMenu(fileName = "WindowsProvider Config", menuName = "Configs/Providers/WindowsSystem/WindowsProvider",
+    order = 0)]
+  public class WindowsProvider : ScriptableObject, IWindowsProvider
   {
     public List<GameObject> windowsObjects;
-    private Dictionary<Type, IWindowBase>  _windows = new();
-    
-    public void OnBeforeSerialize()
+    private Dictionary<Type, IWindowBase> _windows = new();
+
+    private void OnEnable()
     {
-      windowsObjects = windowsObjects.Distinct().ToList();
-      _windows ??= new Dictionary<Type, IWindowBase>();
-      _windows.Clear();
-      
-      for (int i = 0; i < windowsObjects.Count; i++)
-      {
-        if (windowsObjects[i] == null)
-          continue;
-        if (windowsObjects[i].TryGetComponent(out IWindowBase window))
-        {
-          _windows.Add(window.GetType(), window);
-        }
-        else
-          windowsObjects[i] = null;
-      }
+      BuildDictionary();
     }
 
-    public void OnAfterDeserialize()
+    private void OnValidate()
     {
-      windowsObjects = _windows.Values.Select(t=> t.gameObject).ToList();
+      BuildDictionary();
     }
-    
+
     public T GetWindowPrefab<T>() where T : MonoBehaviour, IWindowBase
     {
       return _windows[typeof(T)] as T;
     }
 
-    
+    private void BuildDictionary()
+    {
+      windowsObjects = windowsObjects.Distinct().ToList();
+      _windows ??= new Dictionary<Type, IWindowBase>();
+      _windows.Clear();
+      for (var i = 0; i < windowsObjects.Count; i++)
+      {
+        if (windowsObjects[i] == null) continue;
+        if (windowsObjects[i].TryGetComponent(out IWindowBase window))
+          _windows.Add(window.GetType(), window);
+        else windowsObjects[i] = null;
+      }
+    }
   }
 }
